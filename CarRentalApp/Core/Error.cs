1f8352a79930f9aeef1aae7bf29f9fc56f67f426
Στέφανος Μότσος <stefanosmotsos01@gmail.com>
2026-05-11 21:35:19 +0300

    feat: added custom Exceptions

---
+++ CarRentalApp/Core/Error.cs
@@ -0,0 +1,22 @@
+﻿namespace CarRentalApp.Core
+{
+    public class Error
+    {
+        public Error()
+        {
+        }
+
+        public Error(string? code, string? field, string? message)
+        {
+            Code = code;
+            Field = field;
+            Message = message;
+        }
+
+        public string? Code { get; set; }
+        public string? Field { get; set; }
+        public string? Message { get; set; }
+
+
+    }
+}
---
+++ CarRentalApp/Exceptions/ServerException.cs
@@ -0,0 +1,9 @@
+﻿namespace CarRentalApp.Exceptions
+{
+    public class ServerException : AppException
+    {
+        public ServerException(string code, string message) : base(code, message)
+        {
+        }
+    }
+}
